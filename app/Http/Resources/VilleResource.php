eb5cb0efd3688fdecb54d031624ddb6274e713b9
Marien Mupenda <marien.mupenda@gmail.com>
2022-05-13 13:21:58 +0200

    add ville resource

---
+++ app/Http/Resources/VilleResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
+
+class VilleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'nom' => Str::title($this->nom),
+        ];
+    }
+}

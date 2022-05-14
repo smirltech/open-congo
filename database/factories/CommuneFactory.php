b0c7a85f1dbf1916939b99ce6da415bcc1104662
Marien Mupenda <marien.mupenda@gmail.com>
2022-05-14 12:40:52 +0200

    add database migrations

---
+++ database/factories/CommuneFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Commune>
+ */
+class CommuneFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
